b8b739f34a5bcddf9b13a305159e08a157fa2b8c
Bishnu Dutta <bishnudutta01@gmail.com>
2024-05-23 11:54:09 +0530

    create rest controller

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/get/data")
+    public ResponseEntity<Map<Integer, String>> getData() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Bishnu Dutt");
+        map.put(2, "Piyush Kumar");
+        map.put(3, "Rahul ");
+        return ResponseEntity.ok(map);
+
+
+    }
+
+}
